337218fd96b5c14d2180ae00a65b626876af1616
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-11-15 12:32:19 -0600

    checkpoint-4 completed

--- server.js
+++ server.js
@@ -23,14 +23,16 @@ var Hapi = require('hapi'),
             path: '/templates/{path*}',
             handler: createDirectoryRoute('templates')
         },
-        staticPages: {
+        spa: {
             method: 'GET',
             path: '/{path*}',
-            handler: createDirectoryRoute('/')
+            handler: {
+               file: path.join(__dirname, '/dist/index.html')
+           }
         }
     };
 
-server.route([ routes.css, routes.js, routes.assets, routes.templates, routes.staticPages ]);
+server.route([ routes.css, routes.js, routes.assets, routes.templates, routes.spa ]);
 server.start( onServerStarted );
 
 function onServerStarted() {
