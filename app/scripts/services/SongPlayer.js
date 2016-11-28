3b13d2d60b1fb697ba26e258c9f3a141aab666d0
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-11-28 11:45:07 -0600

    checkpoint-10 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           /**
           * @desc Song player
           * @type {Object}
@@ -28,6 +28,12 @@
               preload: true
             });
 
+            currentBuzzObject.bind('timeupdate', function() {
+                     $rootScope.$apply(function() {
+                         SongPlayer.currentTime = currentBuzzObject.getTime();
+                     });
+                 });
+                 
             SongPlayer.currentSong = song;
           };
 
@@ -63,6 +69,12 @@
           * @type {Object}
           */
           SongPlayer.currentSong = null;
+
+          /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          */
+          SongPlayer.currentTime = null;
 
           SongPlayer.play = function(song) {
             song = song || SongPlayer.currentSong;
@@ -106,10 +118,21 @@
           }
         };
 
+        /**
+         * @function setCurrentTime
+         * @desc Set current time (in seconds) of currently playing song
+         * @param {Number} time
+         */
+         SongPlayer.setCurrentTime = function(time) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.setTime(time);
+             }
+         };
+
       return SongPlayer;
    };
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
